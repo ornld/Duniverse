d0eed9e1ece2f5a177685766f005f9b3096837bb
arnold <notlukearnold@outlook.com>
2026-07-12 09:18:57 -0500

    Changed his chapter introductions wording.

--- obj/Debug/net10.0/Duniverse.AssemblyInfo.cs
+++ obj/Debug/net10.0/Duniverse.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Duniverse")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e34d16b68395611ed912e96939d8c1c59152003")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd69667283788c1d2586059f8e48af406889e462")]
 [assembly: System.Reflection.AssemblyProductAttribute("Duniverse")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Duniverse")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
